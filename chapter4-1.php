dce933c297a8456f93ae615237a6086fc9277d67
tehnn <tehnnn@gmail.com>
2017-07-06 15:14:17 +0700

    ok

--- chapter4-1.php
+++ chapter4-1.php
@@ -1,32 +1,31 @@
 <!DOCTYPE html>
 <html>
 <head>
-<meta charset=utf-8 />
-<title>chapter4-1</title>
-<meta name='viewport' content='initial-scale=1,maximum-scale=1,user-scalable=no' />
-<script src='https://api.mapbox.com/mapbox.js/v3.1.1/mapbox.js'></script>
-<link href='https://api.mapbox.com/mapbox.js/v3.1.1/mapbox.css' rel='stylesheet' />
-<style>
-body { margin:0; padding:0; }
-#map { position:absolute; top:0; bottom:0; width:100%; }
-</style>
+	<meta charset=utf-8 />
+	<title>chapter4-1</title>
+	<meta name='viewport' content='initial-scale=1,maximum-scale=1,user-scalable=no' />
+	<script src='https://api.mapbox.com/mapbox.js/v3.1.1/mapbox.js'></script>
+	<link href='https://api.mapbox.com/mapbox.js/v3.1.1/mapbox.css' rel='stylesheet' />
+
+	
+
+	<style>
+		body { margin:0; padding:0; }
+		#map { position:absolute; top:0; bottom:0; width:100%; }
+	</style>
 </head>
 <body>
-<div id='map'></div>
-<script>
+	<div id='map'></div>
+	<script>
 
-L.mapbox.accessToken = 'pk.eyJ1IjoidGVobm5uIiwiYSI6ImNpZzF4bHV4NDE0dTZ1M200YWxweHR0ZzcifQ.lpRRelYpT0ucv1NN08KUWQ';
-var map = L.mapbox.map('map', 'mapbox.streets');
-map.setView([16.00,100.00],8);
+		L.mapbox.accessToken = 'pk.eyJ1IjoidGVobm5uIiwiYSI6ImNpZzF4bHV4NDE0dTZ1M200YWxweHR0ZzcifQ.lpRRelYpT0ucv1NN08KUWQ';
+		var map = L.mapbox.map('map', 'mapbox.streets');
+		L.control.scale().addTo(map);
+		map.setView([16.00,100.00],6);
 
-var point1 = L.point([16.8123,99.5678]);
-var point2 = L.point([14.635,100.2347]);
-var distance = point1.distanceTo(point2);
-console.log(distance*100);
+		var point1 = L.point([16,100]);
 
-var latlngs = [[point1.x,point1.y],[point2.x,point2.y]];
-var polyline = L.polyline(latlngs, {color: 'red'}).addTo(map);
-
-</script>
+		
+	</script>
 </body>
 </html>
