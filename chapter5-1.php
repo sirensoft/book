9ca6e6fe986ca2421f79119c3b3d7b295d74fd3a
tehnn <tehnnn@gmail.com>
2017-07-08 22:20:06 +0700

    Sat 07/08/2017 22:20:06.60

--- chapter5-1.php
+++ chapter5-1.php
@@ -9,10 +9,32 @@
 	<style>
 		body { margin:0; padding:0; }
 		#map { position:absolute; top:0; bottom:0; width:100%; }
+		#menu {
+			position:absolute;
+			background: #fff;
+			padding: 10px;
+			right: 0;
+    		
+    		
+		}
 	</style>
 </head>
 <body>
 <div id='map'></div>
+<div id='menu'>
+    <input id='mapbox/basic-v9' type='radio' name='rtoggle' value='basic'>
+    <label for='basic'>basis</label>
+    <input id='gisedegem/ciyldbezy005e2ro2b0xz6u4e' type='radio' name='rtoggle' value='streets' checked='checked'>
+    <label for='streets'>stratenplan</label>
+    <input id='mapbox/bright-v9' type='radio' name='rtoggle' value='bright'>
+    <label for='bright'>helder</label>
+    <input id='mapbox/light-v9' type='radio' name='rtoggle' value='light'>
+    <label for='light'>licht</label>
+    <input id='mapbox/dark-v9' type='radio' name='rtoggle' value='dark'>
+    <label for='dark'>donker</label>
+    <input id='mapbox/satellite-v9' type='radio' name='rtoggle' value='satellite'>
+    <label for='satellite'>satelliet</label>
+</div>
 <script>
 L.mapbox.accessToken = 'pk.eyJ1IjoidGVobm5uIiwiYSI6ImNpZzF4bHV4NDE0dTZ1M200YWxweHR0ZzcifQ.lpRRelYpT0ucv1NN08KUWQ';
 var map = L.mapbox.map('map', 'mapbox.streets');
@@ -35,7 +57,7 @@ var geojsonPoint =[
 	},
 	{
 		"type": "Feature",
-		"properties": {	
+		"properties": {				
 			"marker-color":"#6495ED",
 			"marker-symbol":"warehouse",
 		},
@@ -46,6 +68,19 @@ var geojsonPoint =[
 	}
 ];
 myLayer.setGeoJSON(geojsonPoint).addTo(map);
+
+
+var layerList = document.getElementById('menu');
+var inputs = layerList.getElementsByTagName('input');
+
+function switchLayer(layer) {
+    var layerId = layer.target.id;
+    map.setStyle('mapbox://styles/' + layerId);
+}
+
+for (var i = 0; i < inputs.length; i++) {
+    inputs[i].onclick = switchLayer;
+}
 </script>
 
 </body>
