c8d689447639a4c28d037520362eff2361ec28e5
tehnn <tehnnn@gmail.com>
2017-07-08 13:06:59 +0700

    utehn ok

--- chapter4-1.php
+++ chapter4-1.php
@@ -5,6 +5,7 @@
 <title>chapter4-1</title>
 <meta name='viewport' content='initial-scale=1,maximum-scale=1,user-scalable=no' />
 <script src='https://api.mapbox.com/mapbox.js/v3.1.1/mapbox.js'></script>
+
 <link href='https://api.mapbox.com/mapbox.js/v3.1.1/mapbox.css' rel='stylesheet' />
 <style>
 	body { margin:0; padding:0; }
@@ -13,6 +14,7 @@
 </head>
 <body>
 <div id='map'></div>
+<script src='https://api.mapbox.com/mapbox.js/plugins/leaflet-geodesy/v0.1.0/leaflet-geodesy.js'></script>
 <script>
 	L.mapbox.accessToken = 'pk.eyJ1IjoidGVobm5uIiwiYSI6ImNpZzF4bHV4NDE0dTZ1M200YWxweHR0ZzcifQ.lpRRelYpT0ucv1NN08KUWQ';
 	var map = L.mapbox.map('map', 'mapbox.streets');
@@ -20,12 +22,22 @@
 	L.control.scale().addTo(map);
 	var point1 = L.point([16.124536,100.6580]);
 	var marker1 = L.marker([16.0124,100.14785]).addTo(map);	
-	var line1 = L.polyline([[16,100],[15,101],[14.9567,99.24358]],{
+	/*var line1 = L.polyline([[16,100],[15,101],[14.9567,99.24358]],{
 		color:'#DC143C',
 		weight: 5,
 		dashArray:5
 	}).addTo(map);
-	line1.bindPopup("I'm line.");
+	line1.bindPopup("I'm line.");*/
+	var latlngs = [[16,100],[15,101],[14.9567,99.24358],[16,100]];
+	var options = {
+		color:'#FF0000',
+		weight:3,
+		dashArray:5
+	};
+	var pol = L.polygon(latlngs,options).addTo(map);
+	var area = (LGeo.area(pol) / 1000000).toFixed(2);
+	pol.bindPopup("Area ="+area+"km<sup>2</sup>");
 </script>
+
 </body>
 </html>
