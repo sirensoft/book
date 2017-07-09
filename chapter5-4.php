6779996f41da38dca0acc7097d19ad49b648fd92
tehnn <tehnnn@gmail.com>
2017-07-09 18:38:07 +0700

    Sun 07/09/2017 18:38:07.09

--- chapter5-4.php
+++ chapter5-4.php
@@ -19,7 +19,22 @@
 		var map = L.mapbox.map('map', 'mapbox.streets');
 		map.setView([16.00,100.00],6);			
 		var pointLayer = L.mapbox.featureLayer();			
-		pointLayer.loadURL('point_data.geojson').addTo(map);	
+		pointLayer.loadURL('point_data.geojson');
+		pointLayer.on('ready', function() {			
+			pointLayer.eachLayer(function(layer) {				
+				if(layer.feature.properties.home==="1"){					
+					layer.setIcon(L.mapbox.marker.icon({
+						"marker-symbol":"warehouse",
+						"marker-color":"#FFA07A"
+					}));					
+				}
+				if(layer.feature.properties.name){
+					layer.bindPopup(layer.feature.properties.name);
+				}
+			});
+		});
+		pointLayer.addTo(map);
+
 	</script>
 
 </body>
