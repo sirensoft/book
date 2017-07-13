7075dc3f73e1c8afa51c74006f97ba2b3f088446
tehnn <tehnnn@gmail.com>
2017-07-14 00:03:14 +0700

    Fri 07/14/2017  0:03:13.21

--- nodeapp/app.js
+++ nodeapp/app.js
@@ -1,10 +1,12 @@
 var express = require('express')
 var cors = require('cors')
+var morgan = require('morgan')
 var app = express()
 app.use(cors())
+app.use(morgan())
 
 
-var turf = require('turf');
+var turf = require('@turf/turf');
 
 var obj = require('./data/pol_data');
 
@@ -66,7 +68,9 @@ app.get('/geo', function (req, res) {
 
 app.get('/point',function(req,res){
 	var point = turf.point([100, 16.1984])
+  var collection = turf.featureCollection([point])
 	res.json(point)
+  //res.json(collection)
 })
 
 app.get('/file',function(req,res){
@@ -82,7 +86,68 @@ app.get('/test1',function(req,res){
 	res.json(json)
 });
 
+app.get('/test2',function(req,res){
+  
+   var circle_feature= turf.circle([100, 16.1984],60,100,'kilometers');
+   circle_feature_collection = turf.featureCollection([circle_feature]);
+   res.json(circle_feature_collection);
+});
+
+
+app.get('/near',function(req,res){
+  var my_point = {  
+  "geometry": {
+    "type": "Point",
+    "coordinates": [100.373486, 16.836126]
+  }
+};
+var against = {
+  "type": "FeatureCollection",
+  "features": [
+{ "type": "Feature", "properties": { }, "geometry": { "type": "Point", "coordinates": [ 100.373486, 16.836126 ] } },
+{ "type": "Feature", "properties": { }, "geometry": { "type": "Point", "coordinates": [ 100.373968, 16.836193 ] } },
+{ "type": "Feature", "properties": { }, "geometry": { "type": "Point", "coordinates": [ 100.374496, 16.835554 ] } },
+{ "type": "Feature", "properties": { }, "geometry": { "type": "Point", "coordinates": [ 100.373921, 16.835665 ] } },
+{ "type": "Feature", "properties": { }, "geometry": { "type": "Point", "coordinates": [ 100.374799, 16.836701 ] } },
+{ "type": "Feature", "properties": { }, "geometry": { "type": "Point", "coordinates": [ 100.372484, 16.83493 ] } },
+{ "type": "Feature", "properties": { }, "geometry": { "type": "Point", "coordinates": [ 100.376531, 16.837182 ] } },
+{ "type": "Feature", "properties": { }, "geometry": { "type": "Point", "coordinates": [ 100.377478, 16.83733 ] } },
+{ "type": "Feature", "properties": { }, "geometry": { "type": "Point", "coordinates": [ 100.377284, 16.837027 ] } },
+{ "type": "Feature", "properties": { }, "geometry": { "type": "Point", "coordinates": [ 100.376042, 16.837198 ] } },
+{ "type": "Feature", "properties": { }, "geometry": { "type": "Point", "coordinates": [ 100.376407, 16.836483 ] } },
+{ "type": "Feature", "properties": { }, "geometry": { "type": "Point", "coordinates": [ 100.376593, 16.83606 ] } },
+{ "type": "Feature", "properties": { }, "geometry": { "type": "Point", "coordinates": [ 100.376935, 16.836495 ] } },
+{ "type": "Feature", "properties": { }, "geometry": { "type": "Point", "coordinates": [ 100.377012, 16.836095 ] } },
+{ "type": "Feature", "properties": { }, "geometry": { "type": "Point", "coordinates": [ 100.377164, 16.835796 ] } },
+{ "type": "Feature", "properties": { }, "geometry": { "type": "Point", "coordinates": [ 100.376795, 16.835664 ] } },
+{ "type": "Feature", "properties": { }, "geometry": { "type": "Point", "coordinates": [ 100.377517, 16.836157 ] } },
+{ "type": "Feature", "properties": { }, "geometry": { "type": "Point", "coordinates": [ 100.377909, 16.836468 ] } },
+{ "type": "Feature", "properties": { }, "geometry": { "type": "Point", "coordinates": [ 100.377913, 16.836631 ] } },
+{ "type": "Feature", "properties": { }, "geometry": { "type": "Point", "coordinates": [ 100.377583, 16.836817 ] } },
+{ "type": "Feature", "properties": { }, "geometry": { "type": "Point", "coordinates": [ 100.377467, 16.836374 ] } },
+{ "type": "Feature", "properties": { }, "geometry": { "type": "Point", "coordinates": [ 100.378236, 16.836871 ] } },
+{ "type": "Feature", "properties": { }, "geometry": { "type": "Point", "coordinates": [ 100.378286, 16.83653 ] } },
+{ "type": "Feature", "properties": { }, "geometry": { "type": "Point", "coordinates": [ 100.378271, 16.836308 ] } },
+{ "type": "Feature", "properties": { }, "geometry": { "type": "Point", "coordinates": [ 100.378294, 16.836145 ] } },
+{ "type": "Feature", "properties": { }, "geometry": { "type": "Point", "coordinates": [ 100.378364, 16.83585 ] } },
+{ "type": "Feature", "properties": { }, "geometry": { "type": "Point", "coordinates": [ 100.375696, 16.836786 ] } },
+{ "type": "Feature", "properties": { }, "geometry": { "type": "Point", "coordinates": [ 100.375226, 16.836646 ] } },
+{ "type": "Feature", "properties": { }, "geometry": { "type": "Point", "coordinates": [ 100.37511, 16.836879 ] } },
+{ "type": "Feature", "properties": { }, "geometry": { "type": "Point", "coordinates": [ 100.37556, 16.837341 ] } }
+]
+};
+  var nearest_point = turf.nearest(my_point, against);
+  against.features.forEach(function(p){
+     var d = turf.distance(p, my_point, "kilometers")
+     console.log(p.geometry.coordinates+" to "+my_point.geometry.coordinates+" ="+d+"km");
+  });
+ 
+});
+
 //end test
+
+
+
 
 
 
